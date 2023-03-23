0f2de38e5b7982b73c202ad0e94dcc9b68d8702f
franco-caceres-sebastian <francacer15@gmail,com>
2023-03-23 14:57:42 -0300

    API colocado

--- app/app.js
+++ app/app.js
@@ -1,2 +1,34 @@
 
-  
+//   apis 
+// https://rickandmortyapi.com/api
+// https://rickandmortyapi.com/api/character
+
+const API_BASE = "https://rickandmortyapi.com/api"
+const API_CHARACTERS = "https://rickandmortyapi.com/api/character"
+
+const llamadaAPI = fetch(API_CHARACTERS)
+
+llamadaAPI
+    .then((data) => {
+        return data.json();
+    })
+
+    .then((data) => {
+        const $conteiner = document.getElementById("div-conteinerApi")
+        const character = data.results;
+        console.log(data.results);
+        for (let i = 0; i < character.length; i++) {
+            $conteiner.innerHTML +=
+        `div class = "conteiner-2">
+        <img class = "img-4"
+        src=${character[i].image}
+        alt= "imagen de personaje"
+        />
+        <h2>${character[i].name}</h2>
+        <p>Gender:${character[i].gender} </p>
+        <p>Species:${character[i].species} </p> 
+        <p>Status:${character[i].status} </p> 
+        <p>Origin:${character[i].origin.name} </p> 
+        </div>`
+        }
+    })
