cccda0dae64d0a82bacae1fcd4d909e5be2f356f
franco-caceres-sebastian <francacer15@gmail,com>
2023-03-25 20:57:27 -0300

    header terminado

--- app/app.js
+++ app/app.js
@@ -18,7 +18,7 @@ llamadaAPI
         const character = data.results;
         console.log(data.results);
         for (let i = 0; i < character.length; i++) {
-            $conteiner.innerHTML +=`
+            $conteiner.innerHTML += `
             div class = "conteiner-2">
         <img class = "img-4"
         src=${character[i].image}
@@ -34,19 +34,20 @@ llamadaAPI
         }
     })
 
-    function redireccion1() {
-        location.href = "https://rickymortylatino.com/pepinillo-rick-3x03/"
-    }
+function redireccion1() {
+    location.href = "https://rickymortylatino.com/pepinillo-rick-3x03/"
+}
 
-    function redireccion2() {
-        location.href = "https://rickymortylatino.com/los-recuerdos-de-morty-3x08/"
-        
-    }
+function redireccion2() {
+    location.href = "https://rickymortylatino.com/los-recuerdos-de-morty-3x08/"
 
-    function redireccion3() {
-        location.href = "https://rickymortylatino.com/el-episodio-del-tanque-de-acido-4x08/"
-    }
+}
 
-    function redireccion4() {
-        location.href = "https://rickymortylatino.com/"
-    }
+function redireccion3() {
+    location.href = "https://rickymortylatino.com/el-episodio-del-tanque-de-acido-4x08/"
+}
+
+function redireccion4() {
+    location.href = "https://rickymortylatino.com/"
+}
+
